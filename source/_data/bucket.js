abf6be64c07092811687afe8a8250daabcb02434
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2022-11-12 00:13:53 -0600

    Just copy files from Airtable (#131)
    * Just copy files from Airtable
    
    Because:
    
    * Build times are outrageous generating the thumbnails myself
    * The eleventy-img plugin doesn't give me the flexibility I want (to
      limit the height as well as the width of the images)
    
    Solution:
    
    * Just grab the originals and thumbs from Airtable on build, for the
      time being until I have a better idea
    * Drop all the eleventy-img stuff
    * Use 11ty.js templates with pagination from the collection of images

--- source/_data/bucket.js
+++ source/_data/bucket.js
@@ -4,6 +4,7 @@ module.exports = async function() {
   console.log("Fetch starting…")
   bucket = new Bucket()
   await bucket.fetchResults()
+  // await bucket.writeImages()
   console.log("Fetch complete!", {imageCount: bucket.images.length})
 
   return bucket
