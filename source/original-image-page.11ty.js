bf8dc56acbc16ef458887c25e6722db2716eb1b1
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2022-11-11 20:48:21 -0600

    Just copy files from Airtable
    Because:
    
    * Build times are outrageous generating the thumbnails myself
    * The eleventy-img plugin doesn't give me the flexibility I want (to
      limit the height as well as the width of the images)
    
    Solution:
    
    * Just grab the originals and thumbs from Airtable on build, for the
      time being until I have a better idea
    * Drop all the eleventy-img stuff
    * Use 11ty.js templates with pagination from the collection of images

---
+++ source/original-image-page.11ty.js
@@ -0,0 +1,23 @@
+const EleventyFetch = require("@11ty/eleventy-fetch")
+
+class OriginalImage {
+  data() {
+    return {
+      pagination: {
+        alias: "image",
+        data: "bucket.images",
+        size: 1,
+      },
+      permalink: (ctx) => {
+        return ctx.image.original.path
+      },
+    }
+  }
+
+  render(ctx) {
+    console.log(`Rendering original for ${ctx.image.name}`)
+    return EleventyFetch(ctx.image.original.sourceUrl, {type: "buffer"})
+  }
+}
+
+module.exports = OriginalImage
