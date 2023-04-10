e1915b933908aa40917aab72d4450c4ad248d09d
Patrick Byrne <34053+pbyrne@users.noreply.github.com>
2023-04-09 22:11:58 -0500

    fixup! Move source of data off of Airtable

--- source/_redirects.11ty.js
+++ source/_redirects.11ty.js
@@ -11,7 +11,6 @@ class Redirects {
     console.log("Rendering _redirects")
     const redirects = []
     data.bucket.images.map((image) => {
-      console.log("Processing redirect for…", {image, original: image.original, thumbnail: image.thumbnail})
       redirects.push(`/${image.original.url} ${data.filesDomain}/${image.original.source} 200`)
       redirects.push(`/${image.thumbnail.url} ${data.filesDomain}/${image.thumbnail.source} 200`)
       // TODO aliases (for renaming to keep respecting the old URL)
